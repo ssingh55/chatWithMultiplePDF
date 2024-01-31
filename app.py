5a965576c7313d7551beafb6b4dd434ece9c9eda
Shubham Singh <suvamsingh55@gmail.com>
2024-01-31 18:14:27 +0530

    used pypdf2 to read the pdf files and dotenv to load the environment variables google generative ai for generating the content based on user input, and langchain models to saved the uploaded so that langchain google genai model can read the index

---
+++ app.py
@@ -0,0 +1,111 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 1000)
+    chunks = text_splitter.split_text(text)
+    return  chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Act as a PDF reader and provide detailed answers to questions based on the provided context. 
+    Read the given context and thoroughly analyze it to extract all the relevant details. 
+    Answer the question as accurately and comprehensively as possible, 
+    ensuring not to provide any incorrect information. 
+    If the answer is not available in the provided context, 
+    explicitly state that the answer is not available. 
+    Pay attention to the specific question asked and provide all the necessary information to address the question completely.\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    # model = ChatGoogleGenerativeAI(model = "gemini-pro", temperature=0.3)
+    # prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    # chain = load_qa_chain(model, chain_type = "stuff", prompt = prompt)
+    # return chain
+
+    # prompt_template = """
+    # Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    # provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    # Context:\n {context}?\n
+    # Question: \n{question}\n
+
+    # Answer:
+    # """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs = True
+    )
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat with multiple pdf")
+    st.header("Chat with Multiple PDF using GeminiAI")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF files and Click on the submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing...."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+if __name__ == "__main__":
+    main()
